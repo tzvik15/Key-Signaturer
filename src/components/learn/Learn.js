c305d51ca3062129ffbc2d41ef087741bd9b0f89
waterylime46@aol.com <waterylime46@aol.com>
2020-03-26 10:04:48 -0600

    background image

--- src/components/learn/Learn.js
+++ src/components/learn/Learn.js
@@ -68,23 +68,23 @@ function log() {
         <>
         <h3 className ="head">This step by step guide will help you find the key signature to any scale by following a simple, step-by-step series of questions, with minimal need for memorization!</h3>
         <h2 className ="head">Question 1</h2>
-        <p>Is it a Major key or Minor key?</p>
-        <button lassName= "ansBut" onClick={log}>current state</button>
-        <button lassName= "ansBut" onClick={major}>Major</button>
-        <button lassName= "ansBut" onClick={minor}>Minor</button>
+        <p className ="text">Is it a Major key or Minor key?</p>
+        <button className= "ansBut" onClick={log}>current state</button>
+        <button className= "ansBut" onClick={major}>Major</button>
+        <button className= "ansBut" onClick={minor}>Minor</button>
         </>
 
         : 
         state.q1===true && state.major===true && state.q2===false?
         <>
         <h3 className ="head">Now that we have a major scale, it's time to ask ourselves the 2nd question: is it C major or F major?</h3>
-        <button lassName= "ansBut" onClick={cOrF}>Yes</button>
-        <button lassName= "ansBut" onClick={q2f}>No</button>
+        <button className= "ansBut" onClick={cOrF}>Yes</button>
+        <button className= "ansBut" onClick={q2f}>No</button>
         </>
         :
         state.q1===true && state.q2===true && state.isCorF===true?
         <>
-        <p>C and F major are the only two scales that we need to memorize the key signature for. C major has NO key signature (no sharps and no flats), and F major only has Bb in its key signature.</p>
+        <p className ="text">C and F major are the only two scales that we need to memorize the key signature for. C major has NO key signature (no sharps and no flats), and F major only has Bb in its key signature.</p>
         <h4>Once you are done memorizing, click the button!</h4>
         <button className= "ansBut" onClick={reset}><Link to="/">DONE!</Link></button>
         </>
@@ -99,28 +99,28 @@ function log() {
         :
         state.q1===true && state.q2===true && state.q3===true && state.hasFlat===true?
         <>
-        <p>If the scale has the word "flat" in it's name, (and it isn't C or F major), then we know it will have flats in it's key signature. Now we must determine which flats it has. To do so, we must first make sure we know the order of flats. Flats appear in a specific order, which is: B,E,A,D,G,C,F. It is suggested you memorize the order of sharps and flats. Here is a mnemonic device to help: Boromir Eats Arrows, Dies. Gollum Captures Frodo.</p>
-        <p>Once we know the order of flats, we simply go down the order until we find the scale name we are looking for (note for this that the order is referring to flats. This means that B = B flat, E = E flat, etc.). Once we arrived at the target note, we simply add one more flat (by order), and those flats are the key signature for that scale.</p>
+        <p className ="text">If the scale has the word "flat" in it's name, (and it isn't C or F major), then we know it will have flats in it's key signature. Now we must determine which flats it has. To do so, we must first make sure we know the order of flats. Flats appear in a specific order, which is: B,E,A,D,G,C,F. It is suggested you memorize the order of sharps and flats. Here is a mnemonic device to help: Boromir Eats Arrows, Dies. Gollum Captures Frodo.</p>
+        <p className ="text">Once we know the order of flats, we simply go down the order until we find the scale name we are looking for (note for this that the order is referring to flats. This means that B = B flat, E = E flat, etc.). Once we arrived at the target note, we simply add one more flat (by order), and those flats are the key signature for that scale.</p>
         <h3 className ="head">Example:</h3>
-        <p>We are looking at the scale of Db major. Going down the order of flats until we match, we get B,E,A,D. Then, adding one more by order leads us to B,E,A,D,G, and those are the flats that are in the key signature of Db major (and it's relative minor, Bb minor).</p>
+        <p className ="text">We are looking at the scale of Db major. Going down the order of flats until we match, we get B,E,A,D. Then, adding one more by order leads us to B,E,A,D,G, and those are the flats that are in the key signature of Db major (and it's relative minor, Bb minor).</p>
         <button className= "ansBut" onClick={reset}><Link to="/">Got It!</Link></button>
         </>
         :
         state.q1===true && state.q2===true && state.q3===true && state.hasFlat===false?
         <>
-        <p>If the scale does not have the word "flat" in it's name, (and it isn't C or F major), then we know it will have sharps in it's key signature. Now we must determine which sharps it has. To do so, we must first make sure we know the order of sharps. sharps appear in a specific order, which is: F, C, G, D, A, E, B. It is suggested you memorize the order of sharps and flats. Here is a mnemonic device to help: Frodo Can Go Down And Eat Breakfast.</p>
-        <p>Once we know the order of sharps, we need to go a "minor second" interval below our scale name (a minor second is half a step, and one note name change). This will give us another note (so a minor second below G is F#, a minor second below C is B, etc.). Once we found this new name, we go down the order of sharps until we hit a match with this new name, and those are the sharps in the key signature for that scale.</p>
+        <p className ="text">If the scale does not have the word "flat" in it's name, (and it isn't C or F major), then we know it will have sharps in it's key signature. Now we must determine which sharps it has. To do so, we must first make sure we know the order of sharps. sharps appear in a specific order, which is: F, C, G, D, A, E, B. It is suggested you memorize the order of sharps and flats. Here is a mnemonic device to help: Frodo Can Go Down And Eat Breakfast.</p>
+        <p className ="text">Once we know the order of sharps, we need to go a "minor second" interval below our scale name (a minor second is half a step, and one note name change). This will give us another note (so a minor second below G is F#, a minor second below C is B, etc.). Once we found this new name, we go down the order of sharps until we hit a match with this new name, and those are the sharps in the key signature for that scale.</p>
         <h3 className ="head">Example:</h3>
-        <p>We are looking at the scale of A major. Going down a minor second brings us to G#. We then go down the order of sharps until we hit a match, we get F, C, G, and those are the sharps that are in the key signature of A major (and it's relative minor, F# minor).</p>
+        <p className ="text">We are looking at the scale of A major. Going down a minor second brings us to G#. We then go down the order of sharps until we hit a match, we get F, C, G, and those are the sharps that are in the key signature of A major (and it's relative minor, F# minor).</p>
         <button className= "ansBut" onClick={reset}><Link to="/">Got It!</Link></button>
         </>
         :
         state.q1===true && state.major===false?
         <>
-        <p>When searching for the key signature of a minor scale, first we must find the "relative major" of it. A minor scale shares the same key signature of its relative major. </p>
-        <p>To find the relative major, we must move up a "minor third" interval from the original scale name. A minor third interval is defined as a change of 1.5 steps, and TWO note names. This means that first we must determine what will be the note name of the target scale. The note name order is: C, D, E, F, G, A, B, C. So, first we must find our original, minor scale note name (disregarding for the moment and "b" or "#" signs attached to it), and move up 2 names from it.</p>
-        <p>Next, we need to determine if the new note name (the one that is 2 above the original) has and "b" or "#" signs. To do that, we look at the 2nd part of finding a relative major: the 1.5 steps. Once we know the relative major's name, we need to account for the distance: we go back to the original, minor scale name (this time including any "b" or "#"), and we go up 1.5 steps from it. (C + 1.5 = D#, C# + 1.5 = E, Bb + 1.5 = C#, etc.) Once we have determined which sounding note is exactly 1.5 steps above our starting note, we then need to reconcile this note with the name we found in the previous step. This means that we must use "b" and "#" signs to make the note name we arrived in at step 1 SOUND like the note we arrived in at step two. Remember, 2 notes can sound the same but have different names (for example F# sounds like Gb). Also remember that there is a half step between B to C, and between E to F. </p>
-        <p>Here is a complete example of this process: original scale is G minor. Going up 2 note names brings us to B. We go up a step and a half from G to get A#/Bb. Looking at those two options, since we know the note name needs to be B, we choose Bb as the final note, meaning that the relative major to G minor is Bb major.</p>
+        <p className ="text">When searching for the key signature of a minor scale, first we must find the "relative major" of it. A minor scale shares the same key signature of its relative major. </p>
+        <p className ="text">To find the relative major, we must move up a "minor third" interval from the original scale name. A minor third interval is defined as a change of 1.5 steps, and TWO note names. This means that first we must determine what will be the note name of the target scale. The note name order is: C, D, E, F, G, A, B, C. So, first we must find our original, minor scale note name (disregarding for the moment and "b" or "#" signs attached to it), and move up 2 names from it.</p>
+        <p className ="text">Next, we need to determine if the new note name (the one that is 2 above the original) has and "b" or "#" signs. To do that, we look at the 2nd part of finding a relative major: the 1.5 steps. Once we know the relative major's name, we need to account for the distance: we go back to the original, minor scale name (this time including any "b" or "#"), and we go up 1.5 steps from it. (C + 1.5 = D#, C# + 1.5 = E, Bb + 1.5 = C#, etc.) Once we have determined which sounding note is exactly 1.5 steps above our starting note, we then need to reconcile this note with the name we found in the previous step. This means that we must use "b" and "#" signs to make the note name we arrived in at step 1 SOUND like the note we arrived in at step two. Remember, 2 notes can sound the same but have different names (for example F# sounds like Gb). Also remember that there is a half step between B to C, and between E to F. </p>
+        <p className ="text">Here is a complete example of this process: original scale is G minor. Going up 2 note names brings us to B. We go up a step and a half from G to get A#/Bb. Looking at those two options, since we know the note name needs to be B, we choose Bb as the final note, meaning that the relative major to G minor is Bb major.</p>
         <h3 className ="head">Found your relative major? AWESOME! Click the button when you are ready.</h3>
         <button className= "ansBut" onClick={major}>FOUND IT!</button>
         <button className= "ansBut" onClick={log}>current state</button>       
--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -6,7 +6,7 @@ import "../start/start.css"
 function Start() {
     return (
         <>
-        <div className ="body">
+        <div className ="start">
         <h2 className = "hAndW">Hello and Welcome!</h2>
         <p className ="p">This application will help you learn and practice the steps to determining the key signature of every scale! To begin, please choose if you would like to learn the process, or test your knowledge.</p>
         <Button className="btn" ><Link to="/learn">Learn</Link></Button>
