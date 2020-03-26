8c411b58db08d13fcb97a48b91fce2a111d95df4
tzvik15 <tzvik15@yahoo.com>
2020-03-26 09:30:23 -0600

    Updates

---
+++ precache-manifest.b7327569029b58aedb2a7f72570655cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bad8e38722d68eda59994a1bbacb5b0",
+    "url": "/Key-Signaturer/index.html"
+  },
+  {
+    "revision": "0463c4ba1fb9eb758033",
+    "url": "/Key-Signaturer/static/css/main.6f9a9742.chunk.css"
+  },
+  {
+    "revision": "8c34e34a381ecb6290b8",
+    "url": "/Key-Signaturer/static/js/2.a16668c1.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Key-Signaturer/static/js/2.a16668c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0463c4ba1fb9eb758033",
+    "url": "/Key-Signaturer/static/js/main.23536eba.chunk.js"
+  },
+  {
+    "revision": "6db543ea23790f47ea52",
+    "url": "/Key-Signaturer/static/js/runtime-main.f4006529.js"
+  }
+]);
