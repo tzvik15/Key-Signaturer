726d7c5857e2b52a5fa78f3aa0749209d56f59ea
tzvik15 <tzvik15@yahoo.com>
2020-03-30 10:32:41 -0600

    Updates

---
+++ precache-manifest.122ee50dc35ea762b2a3cf9ac970787a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5263b9826cc6c7c28950d2cb3f451f71",
+    "url": "/Key-Signaturer/index.html"
+  },
+  {
+    "revision": "1ddb4ed8250017708fd4",
+    "url": "/Key-Signaturer/static/css/main.d886a400.chunk.css"
+  },
+  {
+    "revision": "0f63ffa56833c16eecb5",
+    "url": "/Key-Signaturer/static/js/2.2d634e85.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Key-Signaturer/static/js/2.2d634e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ddb4ed8250017708fd4",
+    "url": "/Key-Signaturer/static/js/main.abebe652.chunk.js"
+  },
+  {
+    "revision": "6db543ea23790f47ea52",
+    "url": "/Key-Signaturer/static/js/runtime-main.f4006529.js"
+  },
+  {
+    "revision": "042ebceb1348bade126b340339e26876",
+    "url": "/Key-Signaturer/static/media/start3.042ebceb.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Key-Signaturer/precache-manifest.f5fe4bbec77a0228e576ab591648f019.js"
+  "/Key-Signaturer/precache-manifest.122ee50dc35ea762b2a3cf9ac970787a.js"
 );
 
 self.addEventListener('message', (event) => {
